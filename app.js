98f0b3dde5bdce886b5c8d6af92b931052eca6b3
Gustavo Gutierrez <gusguti04@gmail.com>
2020-11-03 14:26:19 -0800

    Create drawCircleBars function. First visual

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ window.onload = function() {
          audioContext = audioContext || new (window.AudioContext || window.webkitAudioContext)();
          audioAnalyser = audioAnalyser || audioContext.createAnalyser();
          audioAnalyser.smoothingTimeConstant = 0.1;
-         audioAnalyser.fftSize = 512;
+         audioAnalyser.fftSize = 512;//2048 -> 1024
          startAudio();
       })
    }
@@ -32,6 +32,8 @@ window.onload = function() {
    function startAudio() {
       source = audioContext.createMediaElementSource(audio);
       source.connect(audioAnalyser);
+      // this connects our music back to the default output,
+      // such as your speakers 
       source.connect(audioContext.destination);
 
       audio.play();
@@ -61,6 +63,37 @@ window.onload = function() {
    canvas.width = window.innerWidth;
    canvas.height = window.innerHeight;
    ctx = canvas.getContext("2d");
+
+   let bars = 200;
+   let barWidth = 2;
+
+   function drawCircleBars(frequencyArr) {
+      // find center of window
+      let centerX = canvas.width / 2;
+      let centerY = canvas.height / 2;
+      let radius = 150;
+
+      //draw circle
+      ctx.beginPath();
+      ctx.arc(centerX,centerY,radius,0,2*Math.PI);
+      ctx.stroke();
+
+      let radians, x, y, xEnd, yEnd, barHeight;
+      for (let i = 0; i < bars; i++) {
+         // divide circle into equal parts
+         radians = (Math.PI * 2) / bars;
+         barHeight = frequencyArr[i] * 0.7;
+
+         //set cordinates
+         x = centerX + Math.cos(radians * i) * (radius);
+         y = centerY + Math.sin(radians * i) * (radius);
+         xEnd = centerX + Math.cos(radius * i) * (radius + barHeight);
+         yEnd = centerY + Math.sin(radius * i) * (radius + barHeight);
+         
+         //draw bar
+         drawBar(x, y, xEnd, yEnd, barWidth, frequencyArr[i]);
+      }
+   }
 
    function drawBar(x1, y1, x2, y2, width, frequency ) {
       let lineColor = "rgb(" + frequency + ", " + frequency + ", " + 205 + ")";
