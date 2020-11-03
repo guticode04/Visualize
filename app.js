b7fdf74ec75b4b0f32671980f3d1905a79d9ebe0
Gustavo Gutierrez <gusguti04@gmail.com>
2020-11-02 18:29:34 -0800

    Add canvas element, add styling and give context

---
+++ app.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+   let canvas, ctx, audio, audioAnalyser, 
+      audioContext;
+
+   canvas = document.getElementById("renderer");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   ctx = canvas.getContext("2d");
+
+
+
+
+}
